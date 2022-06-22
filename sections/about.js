cdd33eee567b2ba1a030f5d2353db4b10f7c37cc
Antoine Bollinger <antoine.bollinger@gmail.com>
2022-06-22 14:34:38 +0200

    💄 section size and bg-colors

--- sections/about.js
+++ sections/about.js
@@ -1,6 +1,6 @@
 export default function About() {
     return (
-        <section className="bg-white border-b" id="about">
+        <section className="bg-white" id="about">
             <div className="container max-w-5xl mx-auto m-8">
                 <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
                     Qui je suis
--- sections/programs.js
+++ sections/programs.js
@@ -1,6 +1,6 @@
 export default function Programs() {
     return (
-        <section className="bg-gray-100" id="programs">
+        <section className="bg-white" id="programs">
             <div className="container mx-auto px-2 pt-4 pb-12 text-gray-800">
                 <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
                     Offres de coaching
@@ -24,9 +24,9 @@ export default function Programs() {
                         </li>
                     </ul>
                 </div>
-                <div className="flex flex-col sm:flex-row justify-between pt-12 my-12 sm:my-4 sm:gap-4">
+                <div className="flex flex-col sm:flex-row justify-between pt-12 my-12 sm:my-4 sm:gap-5">
                     <div className="flex flex-col w-5/6 lg:w-1/3 mx-auto lg:mx-0 rounded-none lg:rounded-l-lg bg-white mt-4">
-                        <div className="flex-1 bg-white text-gray-600 rounded-t rounded-b-none overflow-hidden shadow">
+                        <div className="flex-1 bg-white text-gray-600 rounded-t rounded-b-none overflow-hidden shadow-xl">
                             <div className="w-full p-8 text-3xl font-bold text-center">
                                 Coaching professionnel individuel
                             </div>
@@ -44,14 +44,14 @@ export default function Programs() {
                                 <li className="border-b py-4 font-bold">
                                     10 séances d'une durée de 1h/1h30 espacées de 10 jours (en présentiel, à distance : en visio, téléphone)
                                 </li>
-                                <li className="border-b py-4 font-bold">
+                                <li className="py-4 font-bold">
                                     Des séances en coaching Outdoor : à l'extérieur de votre cadre habituel pour cultiver la mise en mouvement
                                 </li>
                             </ul>
                         </div>
                     </div>
                     <div className="flex flex-col w-5/6 lg:w-1/3 mx-auto lg:mx-0 rounded-none lg:rounded-l-lg bg-white mt-4">
-                        <div className="flex-1 bg-white text-gray-600 rounded-t rounded-b-none overflow-hidden shadow">
+                        <div className="flex-1 bg-white text-gray-600 rounded-t rounded-b-none overflow-hidden shadow-xl">
                             <div className="w-full p-8 text-3xl font-bold text-center">
                                 Coaching Bien-être & perte de poids
                             </div>
@@ -78,27 +78,27 @@ export default function Programs() {
                                 <li className="border-b py-4 font-bold">
                                     Accompagnement sur 5/6 mois, nombre de séances à définir en fonction du besoin, des séances d'une durée de 1h/1h30 espacées de 10 jours (en présentiel, à distance : en visio, téléphone.)
                                 </li>
-                                <li className="border-b py-4 font-bold">
+                                <li className="py-4 font-bold">
                                     Des séances en coaching Outdoor : à l'extérieur de votre cadre habituel pour cultiver la mise en mouvement
                                 </li>
                             </ul>
                         </div>
                     </div>
                     <div className="flex flex-col w-5/6 lg:w-1/3 mx-auto lg:mx-0 rounded-none lg:rounded-l-lg bg-white mt-4">
-                        <div className="flex-1 bg-white text-gray-600 rounded-t rounded-b-none overflow-hidden shadow">
+                        <div className="flex-1 bg-white text-gray-600 rounded-t rounded-b-none overflow-hidden shadow-xl">
                             <div className="w-full p-8 text-3xl font-bold text-center">
                                 Atelier pour enfants : oser s'affirmer par la prise de parole
                             </div>
                             <div className="h-1 w-full gradient my-0 py-0"></div>
                             <ul className="w-full px-2">
                                 <li className="border-b py-4 font-bold">
-                                    # 1 Oser prendre la parole -  Gestion des émotions ( canaliser sa peur , son stress, son appréhension)
+                                    #1 Oser prendre la parole -  Gestion des émotions ( canaliser sa peur , son stress, son appréhension)
                                 </li>
                                 <li className="border-b py-4 font-bold">
-                                    # 2 Développer son assertivité - Oser exprimer son point de vue sans avoir peur du jugement
+                                    #2 Développer son assertivité - Oser exprimer son point de vue sans avoir peur du jugement
                                 </li>
-                                <li className="border-b py-4 font-bold">
-                                    # 3 Développer son estime de soi – Accepter la critique , les désaccords , les points de vue divergeant.
+                                <li className="py-4 font-bold">
+                                    #3 Développer son estime de soi – Accepter la critique , les désaccords , les points de vue divergeant.
                                 </li>
                             </ul>
                         </div>
