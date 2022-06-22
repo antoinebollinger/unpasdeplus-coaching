b6a7abab647d93a9c45b3941df59d7d6dfa73781
Antoine Bollinger <antoine.bollinger@gmail.com>
2022-06-22 15:09:36 +0200

    🐛 trying again reloading

--- components/navbar.js
+++ components/navbar.js
@@ -57,7 +57,7 @@ class Navbar extends Component {
                 <nav id="header" className="fixed w-full z-30 top-0">
                     <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
                         <div className="pl-4 flex items-center">
-                            <a className="toggleColour transition no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="/" id="reload">
+                            <a className="toggleColour transition no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="." id="reload">
                                 UNPASDEPLUS
                             </a>
                         </div>
@@ -72,7 +72,7 @@ class Navbar extends Component {
                         <div className="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 lg:bg-transparent p-4 lg:p-0 z-20" id="nav-content">
                             <ul className="list-reset lg:flex justify-end flex-1 items-center">
                                 <li className="mr-3">
-                                    <a className="toggleColour inline-block py-2 px-4 hover:underline" href="/">Accueil</a>
+                                    <a className="toggleColour inline-block py-2 px-4 hover:underline" href="#home">Accueil</a>
                                 </li>
                                 <li className="mr-3">
                                     <a className="toggleColour inline-block py-2 px-4 hover:underline" href="#about">Qui je suis</a>
