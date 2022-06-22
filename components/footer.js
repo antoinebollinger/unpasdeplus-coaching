82ad734b4991718d27a4066d0da59afedf2b9c6b
Antoine Bollinger <antoine.bollinger@gmail.com>
2022-06-22 12:56:44 +0200

    🐛 unescaped entities to false

--- components/footer.js
+++ components/footer.js
@@ -12,7 +12,7 @@ export default function Footer() {
                         <p className="uppercase text-gray-500 md:mb-6">Partie légale</p>
                         <ul className="list-reset mb-6">
                             <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                                <a href="#" className="no-underline hover:underline text-gray-800 hover:text-pink-500">Conditions d'utilisation</a>
+                                <a href="#" className="no-underline hover:underline text-gray-800 hover:text-pink-500">Conditions dutilisation</a>
                             </li>
                             <li className="mt-2 inline-block mr-2 md:block md:mr-0">
                                 <a href="#" className="no-underline hover:underline text-gray-800 hover:text-pink-500">Politique de confidentialité</a>
