97c5def4b0a6102f5096357e5a7c2a83b6684bee
Antoine Bollinger <antoine.bollinger@gmail.com>
2022-06-23 10:51:55 +0200

    🚧 adding biography

--- pages/bio.js
+++ pages/bio.js
@@ -35,7 +35,7 @@ export default function Bio() {
                                 Ma bio
                             </h3>
                             <p className="text-gray-600 mb-4">
-                                Cela fait plus de dix ans que je veille à manger bio, local et éthique. Je me sens en adéquation avec ces choix de vie qui font mon identité : une personne en mouvement, spirituellement et physiquement !<br />Ainsi, au fil des années, j’ai gagné en sérénité en prenant soin de mon corps et de ma tête.<br />Cela n’a pas toujours été le cas ! Découvrez mon parcours pour comprendre comment je suis parvenue à m’épanouir et à devenir la personne que je suis aujourd’hui.
+                                Cela fait plus de dix ans que je veille à manger bio, local et éthique.Je me sens en adéquation avec ces choix de vie qui font mon identité: une personne en mouvement, spirituellement et physiquement !<br />Ainsi, au fil des années, j’ai gagné en sérénité en prenant soin de mon corps et de ma tête.<br />Cela n’a pas toujours été le cas !Découvrez mon parcours pour comprendre comment je suis parvenue à m’épanouir et à devenir la personne que je suis aujourd’hui.
                             </p>
                         </div>
                     </div>
@@ -46,7 +46,7 @@ export default function Bio() {
                         </div>
                         <div className="w-5/6 sm:w-1/2 p-6 flex flex-col justify-center">
                             <p className="text-gray-600 mb-8">
-                                Passionnée de course à pied, sport que je pratique depuis 20 ans déjà, j’aime les challenges. Que ce soit dans le domaine sportif, professionnel ou personnel. Selon moi, les challenges sont source de motivation et permettent de nous dépasser.
+                                Passionnée de course à pied, sport que je pratique depuis 20 ans déjà, j’aime les challenges.Que ce soit dans le domaine sportif, professionnel ou personnel.Selon moi, les challenges sont source de motivation et permettent de nous dépasser.
                             </p>
                             <p className="italic mx-auto text-center">Ne limite pas tes défis, défie tes limites</p>
                         </div>
@@ -58,7 +58,7 @@ export default function Bio() {
                         </div>
                         <div className="w-5/6 sm:w-1/2 p-6 flex flex-col justify-center">
                             <p className="text-gray-600 mb-8">
-                                Connectée à la nature, aux paysages. Cela m’aide à me ressourcer et à nourrir mon intérieur. Je pratique la marche active et suis adepte des trails, c’est-à-dire des courses en sentier, en milieu naturel. J’accorde également une grande importance à une alimentation saine.
+                                Connectée à la nature, aux paysages.Cela m’aide à me ressourcer et à nourrir mon intérieur.Je pratique la marche active et suis adepte des trails, c’est-à-dire des courses en sentier, en milieu naturel.J’accorde également une grande importance à une alimentation saine.
                             </p>
                             <p className="mx-auto text-center"><span className="italic">L’homme doit harmoniser l’esprit et le corps</span> (Hippocrate)</p>
                         </div>
@@ -77,45 +77,69 @@ export default function Bio() {
                         <h4 className="text-2xl text-gray-800 font-bold leading-none mb-6 text-center">
                             Mes débuts, ma carrière
                         </h4>
-                        <p className="text-gray-600 mb-8">
-                            J'ai intégré le monde de l'entreprise après mes études à l'âge de 23 ans. J'ai évolué dans différents services avec des environnements très différents. Je devais m'adapter à ces changements et faire preuve d'une capacité d'adaptation.
-                            Cela passait parfois par des concessions, faire des choix pas toujours simple... Mais je le DEVAIS pour évoluer professionnellement ! Comme je passait beaucoup de temps dans les transports, au travail, à s'inscrire à des formations internes pour espérer évoluer ! Donc, beaucoup d'investissement personnel pour « Réussir » professionnellement.
-                        </p>
+                        <div class="text-gray-600">
+                            <p class="mb-2">
+                                J'ai intégré le monde de l'entreprise après mes études à l'âge de 23 ans. J'ai évolué dans différents services avec des environnements très différents.Je devais m'adapter à ces changements et faire preuve d'une capacité d'adaptation.
+                            </p>
+                            <p class="mb-2">
+                                Cela passait parfois par des concessions, faire des choix pas toujours simple...Mais je le DEVAIS pour évoluer professionnellement !Comme je passait beaucoup de temps dans les transports, au travail, à s'inscrire à des formations internes pour espérer évoluer ! Donc, beaucoup d'investissement personnel pour « Réussir » professionnellement.
+                            </p>
+                        </div>
                     </div>
                     <div className="p-6">
                         <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3 text-center">
                             Vie pro / vie perso
                         </h3>
                         <h4 className="text-2xl text-gray-800 font-bold leading-none mb-6 text-center">
-                            Comment les conciliers ?
+                            Comment les conciliers?
                         </h4>
-                        <p className="text-gray-600 mb-8">
-                            J'ai intégré le monde de l'entreprise après mes études à l'âge de 23 ans. J'ai évolué dans différents services avec des environnements très différents. Je devais m'adapter à ces changements et faire preuve d'une capacité d'adaptation.
-                            Cela passait parfois par des concessions, faire des choix pas toujours simple... Mais je le DEVAIS pour évoluer professionnellement ! Comme je passait beaucoup de temps dans les transports, au travail, à s'inscrire à des formations internes pour espérer évoluer ! Donc, beaucoup d'investissement personnel pour « Réussir » professionnellement.
-                        </p>
+                        <div class="text-gray-600">
+                            <p class="mb-2">
+                                Mais ce rythme de vie commençait à m’épuiser.Je me suis sentie tiraillée entre mon envie de réussir ma carrière et celle de voir grandir mes enfants.Ma charge mentale s’accroissait et j’ai commencé à remettre en question mes choix professionnels.
+                            </p>
+                            <p class="mb-2">
+                                Très vite, j’ai dû faire face à l’épuisement, à la lassitude, à l’incompréhension.Désillusion dans mes projets professionnels, contraintes qui se faisaient de plus en plus ressentir, et le sentiment que je subissais cette vie.Je me rends donc à l'évidence que je dois trouver une solution plus pérenne pour rééquilibrer ma vie.
+                            </p>
+                            <p class="mb-2">
+                                Je comprends que mes choix de vie ne sont plus alignés à <span className="font-bold">mes valeurs</span>, <span className="font-bold">mon identité</span>, <span className="font-bold">ma personnalité</span>.Je me sens de plus en plus déconnecté de mon milieu professionnel et un besoin de me reconnecter à mes besoins.
+                            </p>
+                        </div>
                     </div>
                     <div className="p-6">
                         <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3 text-center">
                             La trentaine
                         </h3>
                         <h4 className="text-2xl text-gray-800 font-bold leading-none mb-6 text-center">
-                            Transition de vie : Remise en question, nouveau cap
+                            Transition de vie: remise en question, nouveau cap
                         </h4>
-                        <p className="text-gray-600 mb-8">
-                            Arrivée à la trentaine, je ne parvenais plus à trouver ma place dans ces choix de vie qui ne me correspondait pas du tout. Je me sentais en perpétuel conflit avec moi-même et surtout prisonnière de ma vie professionnelle. Je devais faire semblant que tout allait bien, trouver la motivation pour faire des choses qui n’avaient aucun sens pour moi… difficile.
-                            Mais à qui en parler ? Avais-je vraiment envie d’entendre l’avis des autres ? De personnes qui ne savaient pas ce que je ressentais au fond de moi ? Pour entendre des « tu te plains alors que tu as une situation confortable ! » ou des « Démissionne et trouve autre chose ! » ? Facile à dire !
-                            Alors comment trouver une issue à cette situation qui, effectivement, était à tout point de vue pourtant « confortable » : salariée d’une grande entreprise depuis près de 10 ans, 13ème mois, CE, salaire ? Une bonne situation. Stable. Alors pourquoi chercher à changer ? Surtout avec cette ancienneté et des enfants en bas âge à charge !
-                        </p>
+                        <div class="text-gray-600">
+                            <p class="mb-2">
+                                Arrivée à la trentaine, je ne parvenais plus à trouver ma place dans ces choix de vie qui ne me correspondait pas du tout.Je me sentais en perpétuel conflit avec moi-même et surtout prisonnière de ma vie professionnelle.Je devais faire semblant que tout allait bien, trouver la motivation pour faire des choses qui n’avaient aucun sens pour moi...Difficile.
+                            </p>
+                            <p class="mb-2">
+                                Mais à qui en parler?Avais-je vraiment envie d’entendre l’avis des autres?De personnes qui ne savaient pas ce que je ressentais au fond de moi?Pour entendre des « tu te plains alors que tu as une situation confortable ! » ou des « Démissionne et trouve autre chose ! »?Facile à dire !
+                            </p>
+                            <p class="mb-2">
+                                Alors comment trouver une issue à cette situation qui, effectivement, était à tout point de vue pourtant « confortable »: salariée d’une grande entreprise depuis près de 10 ans, 13<sup>ème</sup> mois, CE, salaire?Une bonne situation.Stable.Alors pourquoi chercher à changer?Surtout avec cette ancienneté et des enfants en bas âge à charge !
+                            </p>
+                        </div>
                     </div>
                     <div className="p-6">
                         <h4 className="text-2xl text-gray-800 font-bold leading-none mb-6 text-center">
                             Ma quête de sens
                         </h4>
-                        <p className="text-gray-600 mb-8">
-                            Arrivée à la trentaine, je ne parvenais plus à trouver ma place dans ces choix de vie qui ne me correspondait pas du tout. Je me sentais en perpétuel conflit avec moi-même et surtout prisonnière de ma vie professionnelle. Je devais faire semblant que tout allait bien, trouver la motivation pour faire des choses qui n’avaient aucun sens pour moi… difficile.
-                            Mais à qui en parler ? Avais-je vraiment envie d’entendre l’avis des autres ? De personnes qui ne savaient pas ce que je ressentais au fond de moi ? Pour entendre des « tu te plains alors que tu as une situation confortable ! » ou des « Démissionne et trouve autre chose ! » ? Facile à dire !
-                            Alors comment trouver une issue à cette situation qui, effectivement, était à tout point de vue pourtant « confortable » : salariée d’une grande entreprise depuis près de 10 ans, 13ème mois, CE, salaire ? Une bonne situation. Stable. Alors pourquoi chercher à changer ? Surtout avec cette ancienneté et des enfants en bas âge à charge !
-                        </p>
+                        <div class="text-gray-600">
+                            <p class="mb-2">
+                                Je décide alors de me prendre main.Cela passe par un accompagnement afin que l’on m’aide à donner un autre sens à ma vie professionnelle et, pourquoi pas, à s’engager dans un changement de vie.
+                            </p>
+                            <p class="mb-2">
+                                Des idées émergent et je comprends qu'il est possible d'avoir une vie harmonieuse alignée à notre état intérieur.
+                            </p>
+                            <p class="mb-2">
+                                À l’issue de cet accompagnement, j’ai compris que je pourrais faire de belle chose en étant moi-même et en étant à l’écoute de mes besoins.Je fonce alors, avec pour objectif de redonner un sens à ma vie!
+
+                            </p>
+                        </div>
                     </div>
                     <div className="p-6">
                         <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3 text-center">
@@ -124,16 +148,32 @@ export default function Bio() {
                         <h4 className="text-2xl text-gray-800 font-bold leading-none mb-6 text-center">
                             Ma nouvelle vie
                         </h4>
-                        <p className="text-gray-600 mb-8">
-                            J’ai évidemment des moments de doutes. « Vais-je y arriver ? », « Est-ce que ce n’est pas trop risqué ? ». Mais finalement, je me dis : « Qu’est-ce que je risque ? ».
-                            Plusieurs sentiments se mêlent. L’excitation de ce changement de vie, la peur d’échouer, que cela ne fonctionne pas. Car j’ai tout de même besoin de me sentir en sécurité. Mais finalement, à ce moment-là de ma vie, je ne l’étais pas. Je ne me sentais plus en sécurité intérieurement. Je réalise donc que je suis prête pour le changement.
-                            Je forge mon mental, bien décidée à trouver le chemin de la sérénité :
-                            « Je ne perds jamais. Soit je gagne, soit j’apprends »
-                            (Nelson Mandela)
-                            Je me forme au métier de coaching, un métier d’accompagnement, c’est-à-dire aider les personnes dans le changement, la transition professionnelle et/ou personnelle. Je veux être plus proche de l’humain, aider ces personnes à se réaliser en allant puiser au fond d’elles toutes les ressources dont elles disposent déjà, à se libérer émotionnellement.
-                            Je contribue ainsi, à mon niveau, au bien-être des autres. Je me sens alignée avec les valeurs qui me sont chères: authenticité, intégrité, générosité, pugnacité, respect de soi.
-                            En tant que coach, j’accompagne aujourd'hui les personnes vers UN PAS DE PLUS dans la connaissance de soi pour révéler tout leur potentiel.
-                        </p>
+                        <div class="text-gray-600">
+                            <p class="mb-2">
+                                J’ai évidemment des moments de doutes.
+                            </p>
+                            <p class="mb-2">
+                                « Vais-je y arriver ? », « Est-ce que ce n’est pas trop risqué ? ».
+                            </p>
+                            <p class="mb-2">
+                                Mais finalement, je me dis: « Qu’est-ce que je risque ? ».
+                            </p>
+                            <p class="mb-2">
+                                Plusieurs sentiments se mêlent. L’excitation de ce changement de vie, la peur d’échouer, que cela ne fonctionne pas. Car j’ai tout de même besoin de me sentir en sécurité. Mais finalement, à ce moment-là de ma vie, je ne l’étais pas. Je ne me sentais plus en sécurité intérieurement. Je réalise donc que je suis prête pour le changement.
+                            </p>
+                            <p class="mb-2">
+                                Je forge mon mental, bien décidée à trouver le chemin de la sérénité : « Je ne perds jamais. Soit je gagne, soit j’apprends » (Nelson Mandela).
+                            </p>
+                            <p class="mb-2">
+                                Je me forme au métier de coaching, un métier d’accompagnement, c’est-à-dire aider les personnes dans le changement, la transition professionnelle et/ou personnelle. Je veux être plus proche de l’humain, aider ces personnes à se réaliser en allant puiser au fond d’elles toutes les ressources dont elles disposent déjà, à se libérer émotionnellement.
+                            </p>
+                            <p class="mb-2">
+                                Je contribue ainsi, à mon niveau, au bien-être des autres. Je me sens alignée avec les valeurs qui me sont chères: authenticité, intégrité, générosité, pugnacité, respect de soi.
+                            </p>
+                            <p class="mb-2">
+                                En tant que coach, j’accompagne aujourd'hui les personnes vers UN PAS DE PLUS dans la connaissance de soi pour révéler tout leur potentiel.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </article>
