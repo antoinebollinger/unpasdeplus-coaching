068d95d7041b12bb9d9b483e3ca401bce7166069
Antoine Bollinger <antoine.bollinger@gmail.com>
2022-06-23 11:26:51 +0200

    ⚡️ link size

--- sections/contact.js
+++ sections/contact.js
@@ -11,9 +11,9 @@ export default function Contact() {
                     <div className="w-full mb-4">
                         <div className="h-1 mx-auto bg-white w-1/6 opacity-25 my-0 py-0 rounded-t"></div>
                     </div>
-                    <a href="mailto:sabrina.appriou@hotmail.fr">sabrina.appriou@hotmail.fr</a>
+                    <a href="mailto:sabrina.appriou@hotmail.fr" title="Envoyer un mail à Sabrina" className="text-2xl">sabrina.appriou@hotmail.fr</a>
                     <br />
-                    <a href="tel:+33661794699">06 61 79 46 99</a>
+                    <a href="tel:+33661794699" title="Téléphoner à Sabrina" className="text-2xl">06 61 79 46 99</a>
                 </div>
                 <div className="w-full sm:w-1/2 p-6">
                     <Form />
