cab9381e1c4d15bbd1d5c6cb417ec21297b20fb7
Antoine Bollinger <antoine.bollinger@gmail.com>
2022-06-24 13:00:33 +0200

    💄 bio translate text & image

--- pages/bio.js
+++ pages/bio.js
@@ -7,7 +7,7 @@ import running from '../public/images/running.jpg'
 import nature from '../public/images/nature.jpg'
 import { useEffect, useState } from "react"
 
-import { getSortedPostsData, getPostData } from "../lib/bio"
+import { getSortedBioData } from "../lib/bio"
 
 import bio1 from "../public/bio/1.jpg";
 import bio2 from "../public/bio/2.jpg";
@@ -15,12 +15,8 @@ import bio3 from "../public/bio/3.jpg";
 import bio4 from "../public/bio/4.jpg";
 import bio5 from "../public/bio/5.jpg";
 
-export async function getServerSideProps() {
-    const allPostsData = getSortedPostsData();
-    allPostsData.map(async (post) => {
-        const postData = await getPostData(post.id);
-        return Object.assign(post, { ...postData })
-    });
+export async function getStaticProps() {
+    const allPostsData = await getSortedBioData();
     return {
         props: {
             allPostsData,
@@ -29,7 +25,7 @@ export async function getServerSideProps() {
 }
 
 export default function AboutMe({ allPostsData }) {
-    const [render, setRender] = useState(false);
+
     const slides = [bio1, bio2, bio3, bio4, bio5];
 
     useEffect(() => {
@@ -38,7 +34,7 @@ export default function AboutMe({ allPostsData }) {
                 if (!entry.isIntersecting) return;
                 setTimeout(() => {
                     entry.target.classList.remove('translate-x-3/4');
-                    entry.target.classList.remove('lg:-translate-x-3/4');
+                    entry.target.classList.remove('-translate-x-3/4');
                     entry.target.classList.remove('opacity-0');
                     entry.target.classList.add('translate-x-0');
                     observer.unobserve(entry.target);
@@ -49,9 +45,7 @@ export default function AboutMe({ allPostsData }) {
         document.querySelectorAll('.slide-in')?.forEach(function (li) {
             timelineObserver.observe(li);
         });
-        setRender(true);
-        console.log(slides);
-    }, [])
+    })
 
     return (
         <Layout home={false} title={"Ma bio"}>
@@ -61,12 +55,11 @@ export default function AboutMe({ allPostsData }) {
                     <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
                         Ma bio
                     </h1>
-                    <div className="w-full mb-4">
+                    <div className="w-full mb-8">
                         <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
                     </div>
-                    <Bio />
-                    <hr className="border-b border-gray-100 opacity-25 my-0 py-0" />
-                    <div className="flex flex-wrap md:flex-row-reverse">
+                    <Bio className="mb-8" />
+                    <div className="flex flex-wrap md:flex-row-reverse mb-8">
                         <div className="w-full md:w-1/3 md:pl-6">
                             <Image src={running} className="w-full mx-auto" alt="Personne qui fait du running" />
                         </div>
@@ -77,8 +70,7 @@ export default function AboutMe({ allPostsData }) {
                             <p className="italic mx-auto text-center">Ne limite pas tes défis, défie tes limites</p>
                         </div>
                     </div>
-                    <hr className="border-b border-gray-100 opacity-25 my-0 py-0" />
-                    <div className="flex flex-wrap">
+                    <div className="flex flex-wrap mb-8">
                         <div className="w-full md:w-1/3 md:pr-6">
                             <Image src={nature} className="w-full mx-auto" alt="Représentation de la nature" />
                         </div>
@@ -89,28 +81,27 @@ export default function AboutMe({ allPostsData }) {
                             <p className="mx-auto text-center"><span className="italic">L’homme doit harmoniser l’esprit et le corps</span> (Hippocrate)</p>
                         </div>
                     </div>
-                    <hr className="border-b border-gray-100 opacity-25 my-0 py-0 mb-8" />
                     <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
                         Mon parcours
                     </h1>
-                    <div className="w-full mb-4">
+                    <div className="w-full mb-8">
                         <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
                     </div>
                     {allPostsData.map(({ id, title, subtitle, contentHtml }) => (
-                        <div key={id} id={`id_${slides[+id - 1].src}`}>
+                        <div key={id} className="mb-8">
                             <div className="flex flex-wrap">
-                                <div className="w-full md:w-1/3 md:pr-6">
+                                <div className="w-full md:w-1/3 md:pr-6 slide-in transition duration-300 opacity-0 -translate-x-3/4">
                                     <Image src={slides[+id - 1]} className="w-full mx-auto" alt={title} />
                                 </div>
-                                <div className="w-full md:w-2/3 md:pl-6 text-gray-600 ">
+                                <div className="w-full md:w-2/3 md:pl-6 text-gray-600 slide-in transition duration-300 opacity-0 translate-x-3/4">
                                     <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
                                         {title}
                                     </h3>
                                     <h4 className="text-2xl text-gray-800 font-bold leading-none mb-6">
                                         {subtitle ?? ''}
                                     </h4>
-                                    <div className="slide-in transition duration-300 opacity-0 translate-x-3/4 bio__content"
-                                        dangerouslySetInnerHTML={{ __html: render ? contentHtml : '' }}>
+                                    <div className="bio__content"
+                                        dangerouslySetInnerHTML={{ __html: contentHtml }}>
                                     </div>
                                 </div>
                             </div>
