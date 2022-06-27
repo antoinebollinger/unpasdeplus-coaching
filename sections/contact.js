dd1316250abf4a15ab5657515c6c79f43ab5e4aa
Antoine Bollinger <antoine.bollinger@gmail.com>
2022-06-27 14:44:06 +0200

    🔍️ SEO

--- sections/contact.js
+++ sections/contact.js
@@ -5,7 +5,7 @@ import Form from "../components/form"
 export default function Contact() {
     return (
         <section id="contact">
-            <div className="container mx-auto flex flex-wrap px-8">
+            <div className="container mx-auto flex flex-wrap px-8 items-center">
                 <h2 className="w-full my-2 text-5xl font-bold leading-tight text-center text-white">
                     Contact
                 </h2>
@@ -25,7 +25,7 @@ export default function Contact() {
                         <FontAwesomeIcon icon={faPhone} /> <a href="tel:+33661794699" title="Téléphoner à Sabrina" className="text-2xl hover:text-secondary-200 transition">06 61 79 46 99</a>
                     </p>
                 </div>
-                <div className="w-full md:w-1/2">
+                <div className="w-full md:w-1/2 mt-8 md:mt-0 border-t md:border-t-0 pt-8 md:pt-0">
                     <Form />
                 </div>
             </div>
