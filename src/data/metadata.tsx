7d3684b9b7519d8bde2b69f2bb1c21fe20238793
Antoine Bollinger <antoine.bollinger@gmail.com>
2023-09-06 09:35:29 +0200

    📝 legals

--- src/data/metadata.tsx
+++ src/data/metadata.tsx
@@ -3,6 +3,7 @@ export const siteData = {
     description: 'Un pas de plus, c\'est un accompagnement pour booster le mental des parents et un dispositif d\'accompagnement des entreprises pour prendre soin de la santé physique et mentale de leur salariés parents.',
     author: 'Antoine Bollinger',
     owner: 'Sabrina Appriou',
+    domain: 'unpasdeplus-coaching.fr',
     url: 'https://unpasdeplus-coaching.fr',
     email: process.env.NEXT_PUBLIC_EMAIL_TO
 }
