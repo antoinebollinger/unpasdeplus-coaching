45bc2b2b9cdf131833d2de40372689a8a5ba1b37
Antoine Bollinger <antoine.bollinger@gmail.com>
2023-09-13 09:34:14 +0200

    🚀 deploy manifest

--- app/manifest.tsx
+++ app/manifest.tsx
@@ -5,11 +5,32 @@ export default function manifest(): MetadataRoute.Manifest {
         name: 'Un Pas de Plus website',
         short_name: 'Un Pas de Plus',
         description: 'Le site de votre coach en développement personnel Sabrina Appriou, fondatrice d\'Un Pas De Plus.',
-        start_url: '/',
+        scope: 'https://unpasdeplus-coaching.fr/',
+        start_url: 'https://unpasdeplus-coaching.fr',
         display: 'standalone',
         background_color: '#fff',
         theme_color: '#421556',
         icons: [
+            {
+                src: '/icon-192x192.png',
+                sizes: '192x192',
+                type: 'image/png'
+            },
+            {
+                src: '/icon-256x256.png',
+                sizes: '256x256',
+                type: 'image/png'
+            },
+            {
+                src: '/icon-384x384.png',
+                sizes: '384x384',
+                type: 'image/png'
+            },
+            {
+                src: '/icon-512x512.png',
+                sizes: '512x512',
+                type: 'image/png'
+            },
             {
                 src: '/favicon.ico',
                 sizes: 'any',
