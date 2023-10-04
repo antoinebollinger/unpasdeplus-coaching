4e1f9a85baa7197ffc62d04eba8e7d50d56d89e9
Antoine Bollinger <antoine.bollinger@gmail.com>
2023-10-04 12:01:38 +0200

    📝 legals

--- src/sections/mentions-legales.tsx
+++ src/sections/mentions-legales.tsx
@@ -8,12 +8,11 @@ export default function MentionsLegales() {
                 <h2>Editeur du site&nbsp;:</h2>
 
                 <p>
-                    Le site <strong>{siteData.domain}</strong> est édité par l'entreprise individuelle Appriou Sabrina – 15bis Route de Saint Colombier, 56250 SAINT-NOLFF<br />
+                    Le site <strong>{siteData.domain}</strong> est édité par l'entreprise individuelle Appriou Sabrina – 56250 SAINT-NOLFF<br />
                     Tel.&nbsp;: <strong>+33(0)6 61 79 46 99</strong><br />
                     Courriel&nbsp;: <strong><a href={`mailto:${process.env.NEXT_PUBLIC_EMAIL_TO}`}>{process.env.NEXT_PUBLIC_EMAIL_TO}</a></strong><br />
                     Directrice de la publication&nbsp;: <strong>{siteData.owner}</strong><br />
                     Conception et réalisation&nbsp;: <strong><a href="https://antoinebollinger.fr" target="_blank">antoinebollinger.fr</a></strong><br />
-                    Crédits Photos&nbsp;: Anthony Rouanet, stock.adobe.com, Freepik
                 </p>
 
                 <h2>Hébergement&nbsp;:</h2>
@@ -30,6 +29,10 @@ export default function MentionsLegales() {
 
                 <p>
                     Tous droits réservés. L’accès au site <strong>{siteData.domain}</strong> vous confère un droit d’usage privé et non exclusif de ce site. L’ensemble des éléments édités sur ce site, incluant notamment les textes, photographies, logos, marques, constituent des œuvres au sens du code de la Propriété Intellectuelle. En conséquence La reproduction du contenu de ce site web, en tout ou en partie, est interdite sans la permission écrite de l’auteur.
+                </p>
+
+                <p>
+                    Crédits Photos&nbsp;: Anthony Rouanet, stock.adobe.com, Freepik
                 </p>
 
                 <h2>Données personnelles:</h2>
