1d9a2e87860b2d738914178762cd9180422efcfe
Antoine Bollinger <antoine.bollinger@gmail.com>
2023-10-06 15:38:51 +0200

    🐛 script in next/script

--- src/components/google.tsx
+++ src/components/google.tsx
@@ -4,11 +4,13 @@ export default function GoogleAnalytics() {
     return (
         <>
             <Script
+                id="google_1"
                 strategy="lazyOnload"
                 async
                 src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
             />
             <Script
+                id="google_2"
                 strategy="lazyOnload">
                 {`
                     window.dataLayer = window.dataLayer || [];
