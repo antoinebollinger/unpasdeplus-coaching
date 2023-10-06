fb1afd8ff43f275025699c9801041233c1d132d6
Antoine Bollinger <antoine.bollinger@gmail.com>
2023-10-06 11:53:08 +0200

    🚧 revert temoignage order

--- src/sections/accueil/temoignages.tsx
+++ src/sections/accueil/temoignages.tsx
@@ -59,15 +59,15 @@ export default function Temoignages({ className = '' }: { className?: string }) 
                             {
                                 [
                                     {
+                                        "author": "Delphine P.",
+                                        "tag": "#coachingpertedepoids #motivation #objectifs #coachingsanté&bienêtre #travailintrospection",
+                                        "text": "Après de multiples <strong>échecs</strong> dans mon régime alimentaire plus ou moins équilibré, j’ai décidé de faire appel à un coach pour m’aider à atteindre mes objectifs. Sabrina m’a soutenue et épaulée. Elle m’a vraiment aidée à identifier les problèmes et les résoudre petit à petit. Son écoute a été primordiale.<br/>Grâce au coaching, j’ai réussi à <strong>rebondir</strong> face à la <strong>baisse de motivation</strong> et reprendre ma vie en main.<br/>Je recommande à 100%, un <strong>investissement</strong> qui vaut le coup ! Pour moi, c’est un nouveau départ. Une <strong>expérience qui a changé ma vie !</strong><br/>Juste merci beaucoup Sabrina !"
+                                    },
+                                    {
                                         "author": "Clara L.",
                                         "tag": "#coachingpertedepoids #motivation #objectifs #coachingsanté&bienêtre #travailintrospection",
                                         "text": "Je suis ravie d’avoir été accompagnée par Sabrina qui est très professionnelle. C’est une coach très positive et douce qui rassure et met en confiance !<br/>Sabrina m’a aidée à atteindre mes <strong>objectifs de perte de poids</strong> en débloquant les <strong>barrières psychologiques</strong> qui me freinaient dans ma réussite. Je conseille fortement l’aide d’un coach qui nous aide à <strong>cadrer</strong> nos <strong>objectifs</strong> afin qu’ils soient <strong>atteignables</strong> et <strong>mesurables</strong> dans le temps. Sabrina est disponible et positive, c’est un plaisir de travailler avec elle ! Elle a su m’accompagner <strong>au-delà de mes attentes initiales</strong>."
                                     },
-                                    {
-                                        "author": "Delphine P.",
-                                        "tag": "#coachingpertedepoids #motivation #objectifs #coachingsanté&bienêtre #travailintrospection",
-                                        "text": "Après de multiples <strong>échecs</strong> dans mon régime alimentaire plus ou moins équilibré, j’ai décidé de faire appel à un coach pour m’aider à atteindre mes objectifs. Sabrina m’a soutenue et épaulée. Elle m’a vraiment aidée à identifier les problèmes et les résoudre petit à petit. Son écoute a été primordiale.<br/>Grâce au coaching, j’ai réussi à <strong>rebondir</strong> face à la <strong>baisse de motivation</strong> et reprendre ma vie en main.<br/>Je recommande à 100%, un <strong>investissement</strong> qui vaut le coup ! Pour moi, c’est un nouveau départ. Une <strong>expérience qui a changé ma vie !</strong><br/>Juste merci beaucoup Sabrina !"
-                                    }
                                 ].map((e, i) => (
                                     <div className="embla__slide" key={i}>
                                         <div className="w-full h-full mx-auto flex flex-col justify-center items-center gap-5">
