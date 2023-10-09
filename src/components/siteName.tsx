1396a7d69b1585d6edc5ed400c57ad39c0f4d8d5
Antoine Bollinger <antoine.bollinger@gmail.com>
2023-10-09 15:30:16 +0200

    🐛 next/script id

--- src/components/siteName.tsx
+++ src/components/siteName.tsx
@@ -3,6 +3,7 @@ import Script from "next/script";
 export default function SiteName() {
     return (
         <Script
+            id="sitename"
             type="application/ld+json"
         >
             {
