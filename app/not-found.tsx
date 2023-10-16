48d25b47536d97675ff58d3c655c31406cba9dbe
Antoine Bollinger <antoine.bollinger@gmail.com>
2023-10-16 09:45:31 +0200

    🐛 not-found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,4 +1,4 @@
-// import Link from 'next/link'
+import Link from 'next/link'
 import Layout from '../src/layout'
 import { siteData } from '../src/data/metadata'
 
@@ -14,9 +14,9 @@ export default function NotFound() {
                     <h1 className="h2">Oups, nous ne trouvons pas la page recherchée.</h1>
                     <p>Vérifiez que l'URL saisie ne contient pas d'erreur.</p>
                 </div>
-                {/* <Link href="/" className="main-btn gradient-btn focus:outline-none uppercase">
+                <Link href="/" className="main-btn gradient-btn focus:outline-none uppercase">
                     Aller à la page d'accueil
-                </Link> */}
+                </Link>
             </div>
         </Layout>
     )
