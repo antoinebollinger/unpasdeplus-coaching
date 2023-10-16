3c46b84d08c5b9a81f3cb6019112382465820e1a
Antoine Bollinger <antoine.bollinger@gmail.com>
2023-10-16 08:25:51 +0200

    🚀 not-found deploy

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,15 +1,22 @@
 import Link from 'next/link'
 import Layout from '../src/layout'
+import { siteData } from '../src/data/metadata'
+
+export const metadata = {
+    title: `Erreur 404 | ${siteData.title}`,
+}
 
 export default function NotFound() {
     return (
         <Layout footer={false}>
-            <div className="min-h-[35vh] flex justify-center items-center">
+            <div className="min-h-[35vh] flex justify-center items-center flex-col gap-6">
                 <div>
-                    <h2>Oups</h2>
-                    <p>La page à laquelle vous souhaitez accéder n'a pas été trouvée.</p>
-                    <Link href="/">Revenir à la page d'accueil</Link>
+                    <h1 className="h2">Oups, nous ne trouvons pas la page recherchée.</h1>
+                    <p>Vérifiez que l'URL saisie ne contient pas d'erreur.</p>
                 </div>
+                <Link href="/" className="main-btn gradient-btn focus:outline-none uppercase">
+                    Aller à la page d'accueil
+                </Link>
             </div>
         </Layout>
     )
