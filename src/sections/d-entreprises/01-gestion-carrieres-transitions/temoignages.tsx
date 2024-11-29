665b474649647d2e327d819f6ba62451abd2a735
Antoine Bollinger <antoine.bollinger@gmail.com>
2024-11-29 09:58:37 +0100

    🚧 reviews

--- src/sections/d-entreprises/01-gestion-carrieres-transitions/temoignages.tsx
+++ src/sections/d-entreprises/01-gestion-carrieres-transitions/temoignages.tsx
@@ -8,7 +8,7 @@ export default function Temoignages({ className = "bg-gray-100" }: { className?:
                 <h3 className="h1 text-center">Témoignages</h3>
                 <Reviews reviews={[
                     {
-                        "author": "Anthony Rouanet",
+                        "author": "Anthony",
                         "tag": "#coachinggestioncarrieres",
                         "text": "Il arrive à tout le monde d'avoir des périodes de doutes. Sabrina n'y pas répond, elle a fait bien plus : elle m'a accompagné en toute simplicité et sans jamais porter de jugement à me poser les bonnes questions. Cela ouvre d'autres perspectives et surtout ne pas s'enfermer. Avoir une écoute extérieure à notre cercle habituel à été bénéfique, et que ça a été avec Sabrina fût un plus. Merci Sabrina !"
                     }
